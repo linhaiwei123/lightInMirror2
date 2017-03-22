3d322cd744fe999ab72df101fa163fafffee7e29
qbkivlin <15986711808@163.com>
2017-03-22 15:00:08 +0800

    modify player collision result

--- assets/script/mirror-control.js
+++ assets/script/mirror-control.js
@@ -20,6 +20,11 @@ cc.Class({
         if(other.node.group == "player" || other.node.group == "shadow-player"){
            //如果是自己生成的影子 加标志位 避免重复生成
            //并且要用数组存起来 当本体离开的时候，删除对应的影子
+           if(other.node.group == 'player'){
+               if(other.node.mirrorNames = undefined){
+                   other.node.mirrorNames[this.node.name] = this.node.name;
+               }
+           }
            if(other.node.mirrorName == undefined || other.node.mirrorName != this.node.name){
                let shadowPlayer = cc.instantiate(this.shadowPlayerPrefab);
                shadowPlayer.mirrorName = this.node.name;
@@ -36,10 +41,13 @@ cc.Class({
     },
 
     onCollisionExit: function(other,self){
-        if(other.node.group == "shadow-player" || other.node.group == "player"){
-            if(other.node.mirror == undefined || other.node.mirrorName != this.node.name){
+        if(other.node.group == "shadow-player"){
+            if(other.node.mirror != undefined || other.node.mirrorName == this.node.name){
                 other.node.emit('leave-mirror');
             }
+        }
+        else if(other.node.group == 'player'){
+            
         }
     }
 
